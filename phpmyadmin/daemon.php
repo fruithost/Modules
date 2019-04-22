65a05a3e656387cc334f0604bb031aa13d80322b
Adrian Preuß <adrian.preuss@icloud.com>
2019-04-22 21:46:39 +0200

    First version of PHPMyAdmin

---
+++ phpmyadmin/daemon.php
@@ -0,0 +1,56 @@
+<?php
+	use fruithost\Database;
+	use fruithost\Auth;
+	
+	class PHPMyAdminDaemon {
+		public function __construct() {
+			$users		= Database::fetch('SELECT `username`, `password` FROM `fh_users`', []);
+			$databases	= Database::fetch('SHOW DATABASES');
+			
+			foreach($users AS $user) {
+				$permissions = [];
+				
+				foreach($databases AS $database) {
+					if(preg_match('/^' . $user->username . '_/', $database->Database)) {
+						$permissions[] = $database->Database;
+					}						
+				}
+				
+				if(!empty($permissions)) {
+					$this->createPermissions($user, $permissions);
+				}
+			}
+		}
+		
+		protected function createPermissions($user, $permissions) {
+			$connection = 'localhost';
+			
+			Database::query('CREATE USER IF NOT EXISTS :username@:connection', [
+				'username'		=> $user->username,
+				'connection'	=> $connection
+			]);
+			
+			Database::query('GRANT USAGE ON *.* TO :username@:connection', [
+				'username'		=> $user->username,
+				'connection'	=> $connection
+			]);
+			
+			foreach($permissions AS $database) {
+				Database::query('GRANT ALL PRIVILEGES ON `' . $database . '`.* TO :username@:connection', [
+					'username'		=> $user->username,
+					'connection'	=> $connection
+				]);
+			}
+			
+			Database::query('ALTER USER :username@:connection IDENTIFIED BY :password', [
+				'username'		=> $user->username,
+				'password'		=> $user->password,
+				'connection'	=> $connection
+			]);
+			
+			Database::query('FLUSH PRIVILEGES');
+		}
+	}
+	
+	new PHPMyAdminDaemon();
+?>
--- phpmyadmin/module.php
+++ phpmyadmin/module.php
@@ -1,9 +1,43 @@
 <?php
 	use fruithost\ModuleInterface;
+	use fruithost\Database;
+	use fruithost\Auth;
 	
 	class PHPMyAdmin extends ModuleInterface {
 		public function init() {
+			if(Auth::isLoggedIn()) {
+				$user = Database::single('SELECT `password` FROM `fh_users` WHERE `id`=:id LIMIT 1', [
+					'id'	=> Auth::getID()
+				]);
+				
+				$_SESSION['PMA_single_signon_user']		= Auth::getUsername();
+				$_SESSION['PMA_single_signon_password']	= $user->password;
+				$_SESSION['PMA_single_signon_host']		= DATABASE_HOSTNAME;
+				$_SESSION['PMA_single_signon_port']		= DATABASE_PORT;
+			}
+		}
+		
+		public function frame() {
+			if(Database::count('SHOW GRANTS FOR :username@:connection;', [
+				'username'		=> Auth::getUsername(),
+				'connection'	=> 'localhost'
+			]) <= 0) {
+				return null;
+			}
 			
+			return $this->url('/module/phpmyadmin/www/');
+		}
+		
+		public function content() {
+			?>
+				<div class="jumbotron text-center bg-transparent text-muted">
+					<i class="material-icons">sentiment_very_dissatisfied</i>
+					<h2>No Databases available!</h2>
+					<div class="alert alert-danger d-inline-block mt-4" role="alert">
+						You need a database, if you wan't to use PHPMyAdmin!
+					</div>
+				</div>
+			<?php
 		}
 	}
 ?>

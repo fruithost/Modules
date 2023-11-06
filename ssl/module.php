eea59dbd9b0e271de779bf6e5f8974b8a58d0fd8
Adrian Preuß <adrian.preuss@icloud.com>
2023-11-06 19:54:02 +0100

    Start developing ssl module

--- ssl/module.php
+++ ssl/module.php
@@ -1,9 +1,116 @@
 <?php
 	use fruithost\ModuleInterface;
+	use fruithost\Database;
+	use fruithost\Auth;
+	use fruithost\Button;
+	use fruithost\I18N;
+	use fruithost\HookParameters;
 	
 	class SSL extends ModuleInterface {
+		private $certificates = [];
+		
 		public function init() {
 			
+		}
+		
+		public function preLoad() {
+			$this->certificates = Database::fetch('SELECT `' . DATABASE_PREFIX . 'certificates`.*, `' . DATABASE_PREFIX . 'domains`.`name` AS `name`, `' . DATABASE_PREFIX . 'domains`.`directory` AS `directory` FROM `' . DATABASE_PREFIX . 'certificates` INNER JOIN `' . DATABASE_PREFIX . 'domains` ON `' . DATABASE_PREFIX . 'certificates`.`domain`=`' . DATABASE_PREFIX . 'domains`.`id` AND `' . DATABASE_PREFIX . 'certificates`.`user_id`=`' . DATABASE_PREFIX . 'domains`.`user_id` AND `' . DATABASE_PREFIX . 'certificates`.`user_id`=:user_id', [
+				'user_id'	=> Auth::getID()
+			]);
+			
+			$this->addFilter('MODULE_DOMAIN_LIST_TABLE_NAME', function($html = [], $domain = null) {
+				$crt	= $this->getCertByDomain($domain);
+				$color	= 'danger';
+				$text	= I18N::get('Add SSL-Certificate');
+				
+				/* Is already HTTPS set */
+				if(isset($crt)) {
+					if($crt->time_created !== NULL) {
+						$color	= 'success';
+						$text	= I18N::get('SSL-Certificate');
+					} else {
+						$color	= 'warning';
+						$text	= I18N::get('SSL-Certificate is currently being integrated');
+					}
+				}
+				
+				$html[] = sprintf('<td width="1px"><a href="%2$s" data-content="<small>%1$s</small>" data-toggle="hover" class="text-%3$s small"><i class="material-icons">lock</span></a></td>', $text, $this->url('/module/ssl/' . ($domain == null ? '' : $domain->id)), $color);
+
+				return $html;
+			});
+			
+			$this->addFilter('MODULE_DOMAIN_LIST_NAME', function($data = [], $domain = null) {
+				$crt = $this->getCertByDomain($domain);
+				
+				/* Is already HTTPS set */
+				
+				if(isset($crt)) {
+					if($crt->time_created !== NULL) {
+						$data['link'] = '<a href="https://%1$s/" target="_blank">%1$s</a></td>';
+					}
+				}
+				
+				return $data;
+			});
+			
+			
+			$this->addFilter('MODULE_DOMAIN_LIST_STATUS', function($data = [], $domain = null) {
+				$crt = $this->getCertByDomain($domain);
+				
+				/* Is already HTTPS set */
+				if(isset($crt)) {
+					if($crt->time_created === NULL) {
+						$data = [
+							'text'	=> I18N::get('Pending'),
+							'html'	=> '<span class="text-warning">%s...</span>'
+						];
+					}
+				}
+				
+				return $data;
+			});
+		}
+		
+		public function getCertByDomain($domain) {
+			if(empty($domain)) {
+				return null;
+			}
+			
+			$entry = null;
+			
+			foreach($this->certificates AS $row) {
+				if($row->domain == $domain->id) {
+					$entry = $row;
+					break;
+				}
+			}
+			
+			return $entry;
+		}
+		
+		public function load($submodule = null) {
+			
+		}
+		
+		public function content($submodule = null) {
+			if(!$this->getModules()->hasModule('domains')) {
+				?>
+					<div class="alert alert-danger mt-4" role="alert">
+						<strong><?php I18N::__('Access denied!'); ?></strong>
+						<p class="pb-0 mb-0"><?php I18N::__('You have no permissions for this page.'); ?></p>
+					</div>
+				<?php
+				return;
+			}
+			
+			switch($submodule) {
+				case 'view':
+					require_once('views/view.php');
+				break;
+				default:
+					require_once('views/list.php');
+				break;
+			}
 		}
 	}
 ?>
---
+++ ssl/views/list.php
@@ -0,0 +1,83 @@
+<?php
+	use fruithost\Auth;
+	use fruithost\I18N;
+?>
+<p><?php I18N::__('Add a certificate to your domain to protect information passed between your website and its visitors, providing more trust for your customers and potentially boosting your search engine rankings.'); ?></p>
+<table class="table table-borderless table-striped table-hover">
+	<thead>
+		<tr>
+			<th scope="col" colspan="2"><?php I18N::__('Domain'); ?></th>
+			<th scope="col"><?php I18N::__('Certificate'); ?></th>
+			<th scope="col"><?php I18N::__('Expires'); ?></th>
+			<th scope="col" colspan="2"></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+			foreach($this->certificates AS $certificate) {
+				?>
+					<tr>
+						<td scope="row"><?php print $certificate->name; ?></td>
+						<td>
+							<?php
+								switch($certificate->type) {
+									case 'LETSENCRYPT':
+										I18N::__('Let\'s Encrypt');
+									break;
+									case 'SELFSIGNED':
+										I18N::__('Self-signed');
+									break;
+									case 'CERTIFICATE':
+										I18N::__('Certificate');
+									break;
+								}
+							?>
+						</td>
+						<td>
+							<?php
+								if($certificate->time_created === null) {
+									printf('<span class="text-warning">%s...</span>', I18N::get('Pending'));
+								} else if($certificate->time_created !== null) {
+									if(!file_exists(sprintf('%s%s/%s', HOST_PATH, Auth::getUsername(), $certificate->directory))) {
+										printf('<span class="text-danger">%s</span>', I18N::get('Error'));
+									} else {
+										printf('<span class="text-success">%s</span>', I18N::get('Live'));
+									}
+								}
+							?>
+						</td>
+						<td>
+							<?php
+								if($certificate->days_expiring === null) {
+									printf('<span class="text-warning">%s...</span>', I18N::get('Pending'));
+								} else if($certificate->days_expiring !== null) {
+									if(!file_exists(sprintf('%s%s/%s', HOST_PATH, Auth::getUsername(), $certificate->directory))) {
+										printf('<span class="text-danger">%s</span>', I18N::get('Error'));
+									} else {
+										printf('<span class="text-success">%s</span>', I18N::get('Live'));
+									}
+								}
+							?>
+						</td>
+						<td class="text-right">
+							<a class="delete btn btn-sm btn-primary" href="<?php print sprintf('%s/%s', $this->url('/module/ssl'), $certificate->id); ?>"><?php I18N::__('Manage'); ?></a>
+						</td>
+					</tr>
+				<?php
+			}
+		?>
+	</tbody>
+</table>
+<script type="text/javascript">
+	_watcher_domain = setInterval(function() {
+		if(typeof(jQuery) !== 'undefined') {
+			clearInterval(_watcher_domain);
+			
+			(function($) {
+				$('button[name="action"].delete').on('click', function(event) {
+					$(event.target).parent().parent().find('input[type="checkbox"][name="domain[]"]').prop('checked', true);
+				});
+			}(jQuery));
+		}
+	}, 500);
+</script>

f4fe271d493650d574573edf39f6ea75b0ed3dbf
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2024-08-20 12:24:11 +0200

    Adding Config module

--- config/module.php
+++ config/module.php
@@ -6,19 +6,121 @@
 	use fruithost\Accounting\Auth;
 	
 	class Config extends ModuleInterface {
+		private $tabs		= [
+			'apache2'		=> 'Apache2',
+			'ftp'	    	=> 'ProFTP',
+			'mysql'		    => 'MySQL',
+			'php'			=> 'PHP',
+			'timezones'		=> 'Timezones',
+		];
+		
+		private $file	= null;
+		private $path	= CONFIG_PATH;
+		private $files	= [
+			'apache2'		=> [
+				'global.conf',
+				'panel.conf',
+				'vhosts/user.conf'
+			],
+			'ftp'		=>  [
+				'proftpd.conf',
+				'modules.conf',
+				'sql.conf'
+			],
+			'mysql'		=>  [
+				'global.cnf'
+			],
+			'php'			=>  [
+				'php.ini'
+			],
+			'timezones'		=>  [
+				'timezones.json'
+			],
+		];
+		
 		public function load($submodule = null) : void {
-			$this->addFilter('MODULES_PAGE_CLASSES', function($classes) {
-				$classes[] = 'left';
-
-				return $classes;
+			$this->addFilter('SUBMODULE_NAME', function($text) {
+				if(isset($this->tabs[$text])) {
+					return I18N::get($this->tabs[$text]);
+				}
+				
+				return $text;
 			});
+			
+			$this->addButton((new Button())->setName('reload')->setLabel(I18N::get('Update'))->addClass('btn-outline-primary'));
 		}
 
 
-		public function content() : void {
+		public function content($submodule = null) : void {
+			if(empty($submodule)) {
+				$submodule = array_keys($this->tabs)[0];
+			}
+			
+			$content    = null;
+			$files      = null;
+			
+			if(!empty($this->files[$submodule])) {
+				$files = $this->files[$submodule];
+			}
+			
+            if(isset($_GET['file'])) {
+	            $this->file = $_GET['file'];
+            } else {
+	            $this->file = $files[0];
+            }
+			
+            if(in_array($this->file, $files)) {
+                $path    = sprintf('%s%s/%s', CONFIG_PATH, $submodule, $this->file);
+	            
+	            if(!file_exists($path)) {
+		            $path    = sprintf('%s%s', CONFIG_PATH, $this->file);
+	            }
+                
+                if(file_exists($path)) {
+                    $content = file_get_contents($path);
+                } else {
+                    $content = false;
+                }
+            } else {
+                $content = false;
+            }
+            
+            $this->assign('content', $content);
+			
 			foreach($this->getTemplate()->getAssigns() AS $name => $value) {
 				${$name} = $value;
 			}
+			?>
+				<ul class="nav nav-tabs mb-4">
+					<?php
+						foreach($this->tabs AS $name => $label) {
+							?>
+								<li class="nav-item">
+									<a class="nav-link<?php print ($submodule === $name ? ' active' : ''); ?>" href="<?php print $this->url(sprintf('/module/config/%s', $name)); ?>"><?php I18N::__($label); ?></a>
+								</li>
+							<?php
+						}
+						
+						if(!empty($files)) {
+							?>
+								<li class="nav-item m-auto"></li>
+								<li class="nav-item">
+									<div class="input-group input-group-sm">
+										<span class="input-group-text" id="label"><?php I18N::__('File'); ?>:</span>
+										<select name="file" class="col form-select form-control-sm" aria-label="File" aria-describedby="label">
+											<?php
+												foreach($files AS $file) {
+													printf('<option value="%1$s"%2$s>%1$s</option>', $file, ($file === $this->file ? ' SELECTED' : ''));
+												}
+											?>
+										</select>
+									</div>
+								</li>
+							<?php
+						}
+					?>
+				</ul>
+			<?php
 
 			require_once('views/display.php');
 		}
--- config/views/display.php
+++ config/views/display.php
@@ -1,5 +1,54 @@
 <?php
 	use fruithost\Localization\I18N;
 	use fruithost\UI\Icon;
+	
+	if(isset($error)) {
+		?>
+			<div class="alert alert-danger mt-4" role="alert"><?php print $error; ?></div>
+		<?php
+	} else if(isset($success)) {
+		?>
+			<div class="alert alert-success mt-4" role="alert"><?php print $success; ?></div>
+		<?php
+	}
 ?>
-HI
+<style>
+    .cm-editor {
+        height:     100%;
+        max-height: none;
+        border:     1px solid var(--bs-tertiary-bg);
+    }
+    
+    .cm-scroller {
+        overflow: auto;
+    }
+</style>
+<div class="tab-content">
+	<div class="tab-pane<?php print (empty($tab) ? ' show active' : ''); ?>" id="<?php print (isset($tab) ? $tab : 'unknown'); ?>" role="tabpanel" aria-labelledby="<?php print (isset($tab) ? $tab : 'unknown'); ?>-tab">
+        <?php
+            if($content === null) {
+                ?>
+                    File not exists.
+                <?php
+            } else if($content === false) {
+	            ?>
+                 File not accessible.
+	            <?php
+            } else {
+                ?>
+                    <div id=surface>
+                        <div id="code-editor"></div>
+                    </div>
+                    <script id="editor-content" type="text/plain"><?php print $content; ?></script>
+                <?php
+            }
+        ?>
+	</div>
+</div>
+<script type="text/javascript">
+	let url = '<?php print $this->url(sprintf('/module/config/%s', $submodule)); ?>';
+	document.querySelector('select[name="file"]').addEventListener('change', (event) => {
+		let value				= event.target.value;
+		window.location.href	= url + '?file=' + value;
+	}, true);
+</script>
